1bacd10cdbdb52fd8e150791f63566e09aac73c5
Dimchik00 <valikov_2000@mail.ru>
2024-08-01 18:53:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#1st program
+print((9**0.5)*5)
+#2st program
+print(99.9>99.8 and 1000 != 1000.1)
+#3st program
+print(1234//10%100 + 5678//10%100)
+#4st program
+print(13.42//1 == 42.13*100%42 and 13.42*100%100 == 42.13//1)
